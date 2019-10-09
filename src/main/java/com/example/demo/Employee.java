5b0213a9765eb201ea1cd00322ddd0957896e166
TPCF235004UR <dawitgebre2014@gmail.com>
2019-10-09 16:16:27 -0400

    my first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String first_name;
+    private String last_name;
+    private String SSN;
+    private String DoB;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(String doB) {
+        DoB = doB;
+    }
+}
